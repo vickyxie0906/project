3f667ee8d59b79e9c5fb8321a94fc041a1f6a1e4
vickyxie0906 <vickyxie0906@gmail.com>
2022-12-13 11:10:24 +0800

    modify

--- main.py
+++ main.py
@@ -5,23 +5,22 @@ import requests
 
 
 class Window(tk.Tk):
-    def __init__(self, lottery_name):  # 建構子 (城市字典)
-        super().__init__()  # 呼叫建構子父類別  # 建立視窗內容標題
+    def __init__(self, lottery_name):  
+        super().__init__()  
         title_Label = tk.Label(self, text="台灣彩卷最新開彩結果", fg="#f17432", font=(
             "Arial", 20)).pack(padx=30, pady=20)  # 設定標籤文字的大小 字型 間距
 
-        buttons_frame = tk.Frame(self)  # 建立框架
+        buttons_frame = tk.Frame(self) 
         buttons_frame.pack(padx=50, pady=(0, 30))
 
         # 建立存放按鈕的容器
-        buttons_frame = tk.Frame(self)  # 建立框架
-        buttons_frame.pack(padx=50, pady=(0, 30))  # 左右 上下 間距
-        # 設定GRID的ROW數量
+        buttons_frame = tk.Frame(self) 
+        buttons_frame.pack(padx=50, pady=(0, 30)) 
         grid_row_nums = 2
-        # enumerate會回傳索引值 將數據組合為索引序列 一般用在迴圈 加上.items()回傳 key value
+       
         for index, lname in enumerate(lottery_name.items()):
-            # print(index,key)
-            cname, ename = lname  # key value變數名 #command=self.button_click 註冊
+         
+            cname, ename = lname 
             btn = tk.Button(buttons_frame, text=f"{cname}", bg="#f1e767", font=(
                 "Arial", 15,"bold"), width=8, padx=20, pady=3)
             btn.grid(row=index % grid_row_nums, column=index //
@@ -29,29 +28,28 @@ class Window(tk.Tk):
             btn.bind("<Button>", self.button_click)
 
     def button_click(self, event):
-        # print(dir(event))查event方法
-        # -->type=tkinter button  #說搞點擊後會抓該按鈕['text']字串的資料
+ 
         btn_text = event.widget['text']
         name_list = btn_text.split("\n")
-        cname = name_list[0]  # 抓分割後第0筆資料
+        cname = name_list[0] 
        
      
         try:
          data = ds.get_data(cname)
-        # 去抓ds.get_forcast_data的函式
+  
         except Exception as e:
-            # 出現錯誤訊息
+          
             messagebox.showwarning("錯誤", "糟糕oops....").pack(
                 side=tk.LEFT, padx=30, pady=30)
             return
       
 
-        if hasattr(self, "displayFrame"):  # 檢查有沒有原框架
-            self.displayFrame.destroy()  # 消滅原來顯示資料的框架 重新再建一個
+        if hasattr(self, "displayFrame"):  
+            self.displayFrame.destroy()  
 
         self.displayFrame = ttk.LabelFrame(
             self,  text=cname, borderwidth=2, relief=tk.GROOVE)  # 建立顯示資料的框架 relief按鈕邊框樣式
-        self.displayFrame.pack(fill=tk.BOTH, padx=50, pady=(0, 30))  # 把寬高填滿
+        self.displayFrame.pack(fill=tk.BOTH, padx=50, pady=(0, 30)) 
         tk.Label(self.displayFrame, text=data).pack(padx=10)
 
 
@@ -61,6 +59,5 @@ def main():
     window.mainloop()
 
 
-if __name__ == "__main__":
-   
+if __name__ == "__main__":  
     main()
