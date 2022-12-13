6fbaa6410f904962b09c2bcb970af0f571bc5341
Your Namevickyxie0906 <vickyxie0906@gmail.com>
2022-12-14 00:55:33 +0800

    modify

--- main.py
+++ main.py
@@ -3,30 +3,25 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from tkinter import ttk, messagebox
 import requests
-# pip install pyinstaller 下載打包專案
-# pyinstaller -F .\main.py -n Taiwan_Lottery -i ./winner.ico 打包專案
+
 
 
 class Window(tk.Tk):
     def __init__(self, lottery_name):  
         super().__init__()  
-        # ---------建立背景-------
-        bgImage = Image.open("lotto.jpg")
+        
+        bgImage = Image.open("lotto.png")
         self.tkImage = ImageTk.PhotoImage(bgImage)
-
         mainCanvas = tk.Canvas(self)
         mainCanvas.create_image(
-            -90, -90, anchor=tk.NW, image=self.tkImage)  # self.tkImage要加self否則圖片會消失
+            -30, -30, anchor=tk.NW, image=self.tkImage)  
         mainCanvas.pack(fill=tk.BOTH, expand=True)
-        # ---------建立背景end-------
-        title_Label = tk.Label(mainCanvas, text="台灣彩卷最新開彩結果", fg="#f17432",bg="white", font=(
-            "Arial", 20)).pack(padx=30, pady=30)  # 設定標籤文字的大小 字型 間距
-
         
+        title_Label = tk.Label(mainCanvas, text="台灣彩卷最新開彩結果", fg="#f17432",bg="white", font=(
+            "Arial", 20)).pack(padx=50, pady=50)   
 
-        # 建立存放按鈕的容器
         buttons_frame = tk.Frame(mainCanvas)
-        buttons_frame.pack(padx=50, pady=(0, 30)) 
+        buttons_frame.pack(padx=50, pady=(0, 40), )
         grid_row_nums = 5
        
         for index, lname in enumerate(lottery_name.items()):
@@ -60,7 +55,7 @@ class Window(tk.Tk):
             self.displayFrame.destroy()  
 
         self.displayFrame = ttk.LabelFrame(
-            self,  text=cname, borderwidth=2, relief=tk.GROOVE)  # 建立顯示資料的框架 relief按鈕邊框樣式
+            self,  text=cname, borderwidth=2, relief=tk.GROOVE)  
         self.displayFrame.pack(fill=tk.BOTH, padx=50, pady=(0, 30)) 
         tk.Label(self.displayFrame, text=data).pack(padx=10)
 
@@ -69,6 +64,7 @@ def main():
     window = Window(ds.lottery_name)
     window.title("台灣彩卷")
     window.resizable(0, 0)
+    window.geometry("+450+10")
     window.mainloop()
 
 
