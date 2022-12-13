a60613091b55b700c508a282f48f90e262615bc1
vickyxie0906 <vickyxie0906@gmail.com>
2022-12-13 10:58:15 +0800

    字體

--- datasource.py
+++ datasource.py
@@ -1,166 +1,177 @@
 import requests
 
 from bs4 import BeautifulSoup
-lottery_name = {"賓果賓果": "Bingobingo", "雙贏彩": "Winwin", "威力彩": "Lottery", "_38樂合彩": "Lottery38", "大樂透": "Biglottery", "_49樂合彩": "Lottery49", "今彩539": "Lottery539",
-                "_39樂合彩": "Lottery39", "_3星彩": "Threestar", "_4星彩": "Fourstar"}
+lottery_name = {"賓果賓果": "Bingobingo", "雙贏彩": "Winwin", "威力彩": "Lottery", "38樂合彩": "Lottery38", "大樂透": "Biglottery", "49樂合彩": "Lottery49", "今彩539": "Lottery539",
+                "39樂合彩": "Lottery39", "3星彩": "Threestar", "4星彩": "Fourstar"}
 
 response = requests.get('https://www.taiwanlottery.com.tw/index_new.aspx')
 
 bsObj = BeautifulSoup(response.content, "lxml")
 
-ball = bsObj.find('div', {'id': 'rightdown'}).findAll(
-    'div', class_='ball_tx ball_yellow')
+ball = bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_tx ball_yellow')
+ball_purple = bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_tx ball_purple')
+ball_lemon = bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_tx ball_lemon')
+ball_green = bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_tx ball_green')
+ball_blue = bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_tx ball_blue')
 
-print('賓果賓果 : -------------')
-賓果賓果 = ""
-賓果賓果 += f"開獎日期 : {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[0].text}\n"
+def get_data(lottery):
+    if lottery == '賓果賓果':
+        # print('賓果賓果 : -------------')
+        賓果賓果 = ""
+        賓果賓果 += f"開獎日期 : {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[0].text}\n"
 
+        賓果賓果 += "開出獎號 : "
 
-賓果賓果 += "開出獎號 : "
+        for index in range(0, 20):
 
-for index in range(0, 20):
+            賓果賓果 += f"{ball[index].text}"
 
-    賓果賓果 += f"{ball[index].text}"
+        賓果賓果 += f"\n超級號 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_red')[0].text}\n"
+        賓果賓果 += f"猜大小 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_blue_BB1')[0].text}\n"
+        賓果賓果 += f"猜單雙 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_blue_BB2')[0].text}"
+        return 賓果賓果
 
+    # print('雙贏彩開獎 : -------------')
+    if lottery == "雙贏彩":
+        雙贏彩 = ""
+        雙贏彩 += f"開獎日期:{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[1].text}\n"
 
-賓果賓果 += f"\n超級號 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_red')[0].text}\n"
-賓果賓果 += f"猜大小 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_blue_BB1')[0].text}\n"
-賓果賓果 += f"猜單雙 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_blue_BB2')[0].text}"
+        雙贏彩 += "開出順序 :"
 
-print('雙贏彩開獎 : -------------')
-雙贏彩 = ""
-雙贏彩 += f"開獎日期:{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[1].text}\n"
+        for index in range(0, 12):
 
-雙贏彩 += "開出順序 :"
+            雙贏彩 += f"{ball_blue[index].text}"
 
-for index in range(0, 12):
+        雙贏彩 += "\n大小順序 :"
 
-    雙贏彩 += f"{ball[index].text}"
+        for index in range(12, 24):
 
-雙贏彩 += "\n大小順序 :"
+            雙贏彩 += f"{ball_blue[index].text}"
+        return 雙贏彩
 
-for index in range(12, 24):
+    # print('威力彩開獎 : -------------')
+    if lottery == "威力彩":
+        威力彩 = ""
+        威力彩 += f"開獎日期 :{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[2].text}\n"
 
-    雙贏彩 += f"{ball[index].text}"
+        威力彩 += "開出順序 :"
 
-print('威力彩開獎 : -------------')
-威力彩 = ""
-威力彩 += f"開獎日期 :{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[2].text}\n"
+        for index in range(0, 6):
 
-威力彩 += "開出順序 :"
+            威力彩 += f"{ball_green[index].text}"
 
-for index in range(0, 6):
+        威力彩 += "\n大小順序:"
 
-    威力彩 += f"{ball[index].text}"
+        for index in range(6, 12):
 
+            威力彩 += f"{ball_green[index].text}"
 
-威力彩 += "\n大小順序:"
+        威力彩 += f"\n特別號: {bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_red')[1].text}"
+        return 威力彩
 
-for index in range(6, 12):
+    # print('_38樂合彩開獎 : -------------')
+    if lottery == "38樂合彩":
+        _38樂合彩 = ""
+        _38樂合彩 += f"開獎日期: {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[3].text}\n"
 
-    威力彩 += f"{ball[index].text}"
+        _38樂合彩 += "開出順序:"
 
-威力彩 += f"\n特別號: {bsObj.find('div', {'id': 'rightdown'}).findAll('div', class_='ball_red')[1].text}"
+        for index in range(12, 18):
 
-print('_38樂合彩開獎 : -------------')
-_38樂合彩 = ""
-_38樂合彩 += f"開獎日期: {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[3].text}\n"
+            _38樂合彩 += f"{ball_green[index].text}"
 
-_38樂合彩 += "開出順序:"
+        _38樂合彩 += "\n大小順序:"
+        for index in range(18, 24):
 
-for index in range(12, 18):
+            _38樂合彩 += f"{ball_green[index].text}"
+        return _38樂合彩
 
-    _38樂合彩 += f"{ball[index].text}"
+    # print('大樂透開獎 : -------------')
+    if lottery == "大樂透":
+        大樂透 = ""
+        大樂透 += f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[4].text}\n"
 
+        大樂透 += "開出順序:"
+        for index in range(20, 26):
 
-_38樂合彩 += "\n大小順序:"
-for index in range(18, 24):
+            大樂透 += f"{ball[index].text}"
 
-    print(ball[index].text, end=' ')
+        大樂透 += "\n大小順序: "
+        for index in range(26, 32):
 
+            大樂透 += f"{ball[index].text}"
 
-print('大樂透開獎 : -------------')
-大樂透 = ""
-大樂透 += f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[4].text}\n"
+        大樂透 += f"\n特別號 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_red')[2].text}"
+        return 大樂透
 
-大樂透 += "開出順序:"
-for index in range(20, 26):
+    # print('_49樂合彩開獎 : -------------')
+    if lottery == "49樂合彩":
+        _49樂合彩 = ""
+        _49樂合彩 += f"開獎日期: { bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[5].text}\n"
 
-    大樂透 += f"{ball[index].text}"
+        _49樂合彩 += "開出順序: "
+        for index in range(32, 38):
 
-大樂透 += "\n大小順序: "
-for index in range(26, 32):
+            _49樂合彩 += f"{ball[index].text}"
 
-    大樂透 += f"{ball[index].text}"
+        _49樂合彩 += "\n大小順序:"
+        for index in range(38, 44):
 
-大樂透 += f"特別號 :{bsObj.find('div',{'id':'rightdown'}).findAll('div',class_='ball_red')[2].text}"
+            _49樂合彩 += f"{ball[index].text}"
+        return _49樂合彩
 
-print('_49樂合彩開獎 : -------------')
-_49樂合彩=""
-_49樂合彩+=f"開獎日期: { bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[5].text}\n"
+    # print('今彩539開獎 : -------------')
+    if lottery == "今彩539":
+        今彩539 = ""
+        今彩539 += f"開獎日期: {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[6].text}\n"
 
-_49樂合彩 += "開出順序: "
-for index in range(32, 38):
+        今彩539 += "開出順序: "
+        for index in range(0, 5):
 
-  _49樂合彩 += f"{ball[index].text}"
+            今彩539 += f"{ball_lemon[index].text}"
 
+        今彩539 += "\n大小順序: "
+        for index in range(5, 10):
 
-_49樂合彩 += "\n大小順序:"
-for index in range(38, 44):
+            今彩539 += f"{ball_lemon[index].text}"
 
-   _49樂合彩 += f"{ball[index].text}"
+        return 今彩539
 
-print('今彩539開獎 : -------------')
-今彩539=""
-今彩539+=f"開獎日期: {bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[6].text}\n"
+    # print('_39樂合彩開獎 : -------------')
+    if lottery == "39樂合彩":
+        _39樂合彩 = ""
+        _39樂合彩 += f"開獎日期:{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[7].text}\n"
 
-今彩539 += "開出順序: "
-for index in range(0, 5):
+        _39樂合彩 += "開出順序 :"
 
-    今彩539 += f"{ball[index].text}"
+        for index in range(10, 15):
 
-今彩539 += "\n大小順序: "
-for index in range(5, 10):
+            _39樂合彩 += f"{ball_lemon[index].text}"
 
-    今彩539 += f"{ball[index].text}"
+        _39樂合彩 += "\n大小順序: "
+        for index in range(15, 20):
 
-print('_39樂合彩開獎 : -------------')
-_39樂合彩=""
-_39樂合彩 += f"開獎日期:{bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[7].text}\n"
+            _39樂合彩 += f"{ball_lemon[index].text}"
+        return _39樂合彩
 
-_39樂合彩 += "開出順序 :"
+    # print('_3星彩開獎 : -------------')
+    if lottery == "3星彩":
+        _3星彩 = ""
+        _3星彩 += f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[8].text}\n"
 
-for index in range(10, 15):
+        _3星彩 += "中獎號碼: "
+        for index in range(0, 3):
 
-    _39樂合彩 += f"{ball[index].text}"
+            _3星彩 += f"{ball_purple[index].text} "
+        return _3星彩
 
-_39樂合彩 += "\n大小順序: "
-for index in range(15, 20):
+    # print('_4星彩開獎 : -------------')
+    if lottery == "4星彩":
+        _4星彩 = ""
+        _4星彩 += f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[9].text}\n"
 
-    _39樂合彩 += f"{ball[index].text}"
+        _4星彩 += "中獎號碼: "
+        for index in range(3, 7):
 
-print('_3星彩開獎 : -------------')
-_3星彩=""
-_3星彩+=f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[8].text}\n"
-
-_3星彩 += "中獎號碼:"
-for index in range(0, 3):
-
-   _3星彩 += f"{ball[index].text}"
-
-print('_4星彩開獎 : -------------')
-_4星彩 = ""
-_4星彩 += f"開獎日期 :{ bsObj.find('div', {'id': 'rightdown'}).findAll('span', class_='font_black15')[9].text}\n"
-
-_4星彩 += "中獎號碼:"
-for index in range(3, 7):
-
-   _4星彩 += f"{ball[index].text}"
-
-
-
-
-
-
-
-
+            _4星彩 += f"{ball_purple[index].text} "
+        return _4星彩
--- main.py
+++ main.py
@@ -7,8 +7,8 @@ import requests
 class Window(tk.Tk):
     def __init__(self, lottery_name):  # 建構子 (城市字典)
         super().__init__()  # 呼叫建構子父類別  # 建立視窗內容標題
-        title_Label = tk.Label(self, text="台灣彩卷最新開彩結果", font=(
-            "Arial", 20)).pack(padx=30, pady=30)  # 設定標籤文字的大小 字型 間距
+        title_Label = tk.Label(self, text="台灣彩卷最新開彩結果", fg="#f17432", font=(
+            "Arial", 20)).pack(padx=30, pady=20)  # 設定標籤文字的大小 字型 間距
 
         buttons_frame = tk.Frame(self)  # 建立框架
         buttons_frame.pack(padx=50, pady=(0, 30))
@@ -22,8 +22,8 @@ class Window(tk.Tk):
         for index, lname in enumerate(lottery_name.items()):
             # print(index,key)
             cname, ename = lname  # key value變數名 #command=self.button_click 註冊
-            btn = tk.Button(buttons_frame, text=f"{cname}", font=(
-                "Arial", 15), width=8, padx=20, pady=3)
+            btn = tk.Button(buttons_frame, text=f"{cname}", bg="#f1e767", font=(
+                "Arial", 15,"bold"), width=8, padx=20, pady=3)
             btn.grid(row=index % grid_row_nums, column=index //
                      grid_row_nums, padx=5, pady=5)
             btn.bind("<Button>", self.button_click)
@@ -32,25 +32,19 @@ class Window(tk.Tk):
         # print(dir(event))查event方法
         # -->type=tkinter button  #說搞點擊後會抓該按鈕['text']字串的資料
         btn_text = event.widget['text']
-        # widget抓按鈕文字 #屬性widget=button,抓資料["text"]取出文字
-        # split分割 #說實體的方法要重新抓取按鈕文字 所以要用下一行來分割字串抓取
         name_list = btn_text.split("\n")
         cname = name_list[0]  # 抓分割後第0筆資料
-        # ename = name_list[1]  # 抓分割後第1筆資料
-        # print(f"{cname}-{ename}")
-
-        # try:
-        #  city_forcase = ds.get_forcast_data(ename, api_key)
-        # # 去抓ds.get_forcast_data的函式
-        # except Exception as e:
-        #     # 出現錯誤訊息
-        #     messagebox.showwarning("錯誤", "糟糕oops....").pack(
-        #         side=tk.LEFT, padx=30, pady=30)
-        #     return
-        # 如果出錯就會跳出警告視窗
-        # print(cname)
-
-        # print(city_forcase)
+       
+     
+        try:
+         data = ds.get_data(cname)
+        # 去抓ds.get_forcast_data的函式
+        except Exception as e:
+            # 出現錯誤訊息
+            messagebox.showwarning("錯誤", "糟糕oops....").pack(
+                side=tk.LEFT, padx=30, pady=30)
+            return
+      
 
         if hasattr(self, "displayFrame"):  # 檢查有沒有原框架
             self.displayFrame.destroy()  # 消滅原來顯示資料的框架 重新再建一個
@@ -58,13 +52,7 @@ class Window(tk.Tk):
         self.displayFrame = ttk.LabelFrame(
             self,  text=cname, borderwidth=2, relief=tk.GROOVE)  # 建立顯示資料的框架 relief按鈕邊框樣式
         self.displayFrame.pack(fill=tk.BOTH, padx=50, pady=(0, 30))  # 把寬高填滿
-        tk.Label(self.displayFrame, text=ds.威力彩).pack(padx=10)
-
-
-# class DisplayFrame(ttk.LabelFrame):  # 繼承ttk.LabelFrame父類別
-
-#     def __init__(self, parent, data=None, **kwargs):  # **kwargs是打包上面DisplayFrame()裡text之後的內容
-#         super().__init__(parent, **kwargs)
+        tk.Label(self.displayFrame, text=data).pack(padx=10)
 
 
 def main():
@@ -74,5 +62,5 @@ def main():
 
 
 if __name__ == "__main__":
-    # print("這裡是程式的執行點")#印出訊息
+   
     main()
